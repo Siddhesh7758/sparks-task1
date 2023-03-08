e3f610b7c81bb3a48285f74f23cd22b6caaa73d4
siddhesh munfan <munfansiddhesh7758@gmail.com>
2023-03-08 13:05:12 +0530

    2nd phase

--- screens/GoogleLogin.js
+++ screens/GoogleLogin.js
@@ -1,8 +1,7 @@
-import { View, Text, TouchableOpacity, Image } from 'react-native'
+import { View, Text, TouchableOpacity, Image, Button } from 'react-native'
 import React, {useEffect, useState} from 'react'
 import {GoogleSignin, GoogleSigninButton} from '@react-native-google-signin/google-signin';
-import auth from '@react-native-firebase/auth';
-
+import auth from '@react-native-firebase/auth'
 
 GoogleSignin.configure({
   webClientId: '254115731633-idptf1cjo263v1f0ei6h0s27qq5kgcjs.apps.googleusercontent.com',
@@ -26,11 +25,14 @@ const GoogleLogin = () => {
 
   const onGoogleButtonPress = async () => {
     
-    await GoogleSignin.hasPlayServices({ showPlayServicesUpdateDialog: true });
-    const { idToken } = await GoogleSignin.signIn();
-    const googleCredential = auth.GoogleAuthProvider.credential(idToken);
-    return auth().signInWithCredential(googleCredential);
-    
+    try {
+      await GoogleSignin.hasPlayServices({ showPlayServicesUpdateDialog: true });
+      const { idToken } = await GoogleSignin.signIn();
+      const googleCredential = auth.GoogleAuthProvider.credential(idToken);
+      return auth().signInWithCredential(googleCredential);
+    } catch (error) {
+      console.log(error) 
+    }
   }
   
 
@@ -38,11 +40,11 @@ const GoogleLogin = () => {
   const signOut = async () => {
     try 
     {
+      await GoogleSignin.revokeAccess();
       auth().signOut().then(() => 
       {
         console.log('User signed out!');
       });
-
     }catch (error){
       console.log(error);
     }
@@ -55,11 +57,16 @@ if (!user) {
       <View className='flex-1 justify-center items-center'>
         <Text className='font-bold text-3xl text-center m-5'>Login to continue...</Text>
 
-        <GoogleSigninButton style={{ width: 192, height: 48 }}
+        <GoogleSigninButton style={{ width: 194, height: 60 }}
           size={GoogleSigninButton.Size.Wide}
           color={GoogleSigninButton.Color.Dark}
           onPress={onGoogleButtonPress}
         />
+
+        <TouchableOpacity className='flex flex-row justify-center items-center mt-4 bg-blue-500 px-2 py-3'>
+        <Image className='' source={{uri : 'https://img.icons8.com/fluency/48/null/facebook-new.png'}} height={30} width={30} />
+        <Text className='text-white mx-1 font-semibold'>Sign in with facebook</Text>
+        </TouchableOpacity>
       </View>
     );
   }
